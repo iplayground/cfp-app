6feb73e0f3c0bdeb26ec7ccd961125a3470d306b
johnlinvc <johnlinvc@gmail.com>
2018-06-20 08:52:55 +0800

    Revert "add env_var OMNIAUTH_FULL_HOST"
    This reverts commit 72798b6f276e6084289363351b1a7259e899003b.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :developer unless Rails.env.production?
 end
-
-OmniAuth.config.full_host = ENV.fetch("OMNIAUTH_FULL_HOST", 'your.host.com')
