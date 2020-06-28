35452abb3f4965193538a50ece6d9ea17c1a840b
johnlinvc <johnlinvc@gmail.com>
2020-06-28 22:14:37 +0800

    fix typo

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -2,7 +2,7 @@ class CommentDecorator < ApplicationDecorator
   delegate_all
   def visible?
     is_admin = h.current_user.admin?
-    is_organizer = h.currnt_user.organizer_events.include?(object.proposal.event)
+    is_organizer = h.current_user.organizer_events.include?(object.proposal.event)
     is_commenter = h.current_user == object.person
     is_admin || is_organizer || is_commenter
   end
