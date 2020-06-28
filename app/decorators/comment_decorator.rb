b36c1f4775ca86cafb01c53b330e98309c083b83
johnlinvc <johnlinvc@gmail.com>
2020-06-28 22:12:15 +0800

    fix usage of decorator

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -2,8 +2,8 @@ class CommentDecorator < ApplicationDecorator
   delegate_all
   def visible?
     is_admin = h.current_user.admin?
-    is_organizer = h.currnt_user.organizer_events.include?(proposal.event)
-    is_commenter = h.current_user == self.person
+    is_organizer = h.currnt_user.organizer_events.include?(object.proposal.event)
+    is_commenter = h.current_user == object.person
     is_admin || is_organizer || is_commenter
   end
 end
